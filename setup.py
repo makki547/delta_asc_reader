20e686d91478e8d9ba3a297869550ae815ef2706
tamaki <makki.ipn@gmail.com>
2021-12-03 09:24:54 +0900

    setup.py was modified.

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"delta_asc_reader": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=['src'],
     python_requires=">=3.6",
 )
